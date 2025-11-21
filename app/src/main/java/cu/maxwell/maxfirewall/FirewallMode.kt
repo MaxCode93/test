ae14f72f32cf57dcb735da027874fd53cf01e421
Carlos Almaguer Sosa <45721198+MaxCode93@users.noreply.github.com>
2025-11-21 02:39:04 -0500

    Delete app directory

--- app/src/main/java/cu/maxwell/maxfirewall/FirewallMode.kt
+++
@@ -1,9 +0,0 @@
-package cu.maxwell.maxfirewall
-
-/**
- * Defines the operating mode for the firewall.
- */
-enum class FirewallMode(val key: String) {
-    VPN("vpn_prefs")
-}
-
--- app/src/main/java/cu/maxwell/maxfirewall/ThemeUtils.kt
+++
@@ -1,9 +0,0 @@
-package cu.maxwell.maxfirewall
-
-import androidx.appcompat.app.AppCompatDelegate
-
-object ThemeUtils {
-    fun applyTheme(themeMode: ThemeMode) {
-        AppCompatDelegate.setDefaultNightMode(themeMode.nightMode)
-    }
-}
